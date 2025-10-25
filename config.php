40dbb1ef9cc7d622882339046d21e96992f4811b
Tim <37852859+uzxans@users.noreply.github.com>
2025-10-25 01:50:13 -0700

    Update config.php
    delete

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'db_name' => 'f0256723_srmglobal',
+    'db_name' => '*',
     'db_host' => 'localhost',
-    'db_user' => 'f0256723_srmglobal',
-    'db_pass' => 'Sardor1993',
+    'db_user' => '*',
+    'db_pass' => '*',
 ];
 ?>
 
